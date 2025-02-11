755a15480533e4597246d2cfe9bed8a5625735cd
Pierson <fernandespierson03@gmail.com>
2025-02-11 22:49:26 +0530

    fixed mongodb connection code which was giving warning

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(limiter);
 app.use('/uploads', express.static('uploads'));
 
 // MongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGO_URI) // Removed deprecated options
   .then(() => console.log("MongoDB connected"))
   .catch(err => console.error("MongoDB connection error:", err));
 
